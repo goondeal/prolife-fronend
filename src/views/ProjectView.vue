329dc5f2fd65ed8b258244f04085d68b5a10341b
ahmad <ahmed.salamony497@gmail.com>
2023-09-23 07:47:12 +0300

    Enable project deletion

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="m-auto p-8 h-full">
-        <h2 class="text-xl font-medium text-gray-900 dark:text-white">Pro-{{ route.params.projectID }}</h2>
+        <div class="flex items-center">
+            <h2 class="text-xl font-medium text-gray-900 dark:text-white">Pro-{{ route.params.projectID }}</h2>
+            <div class="w-8"></div>
+            <TrashIcon class="rounded-full p-1 h-8 w-8 text-red-500 hover:bg-gray-200"
+            @click="deleteProject()"></TrashIcon>
+
+        </div>
         <div v-if="project._tasks" class="my-4">
             <div class="flex">
                 
@@ -15,16 +21,18 @@
   
 <script setup>
 import { ref, onMounted, watch } from 'vue'
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import useAuthRequest from '../composables/useAuthRequest'
+import { TrashIcon,  } from '@heroicons/vue/24/outline'
 import DataTable from '../components/DataTable.vue'
 
 
 const route = useRoute()
+const router = useRouter()
 const { sendAuthRequest } = useAuthRequest()
 const project = ref({})
 
-const getCategoryData = () => {
+const getProjectData = () => {
     sendAuthRequest({ url: `/categories/${route.params.id}/projects/${route.params.projectID}`, method: 'get' })
         .then((response) => {
             console.log('project retrieve response = ', response)
@@ -37,15 +45,24 @@ const getCategoryData = () => {
                     })
                 }
             }
-        }).catch((err) => console.log('category retrieve err = ', err))
+        }).catch((err) => console.log('project retrieve err = ', err))
 }
 
 onMounted(() => {
-    getCategoryData(),
+    getProjectData(),
         watch(
-            () => route.params.id,
-            getCategoryData,
+            () => route.params.projectID,
+            getProjectData,
         )
 })
 
+const deleteProject = () => {
+    sendAuthRequest({ url: `/categories/${route.params.id}/projects/${route.params.projectID}`, method: 'delete' })
+        .then((response) => {
+            console.log('project delete response = ', response)
+            if (response.status === 204) {
+                router.replace({name: 'category-detail', params: {id: route.params.id}})
+            }
+        }).catch((err) => console.log('project delete err = ', err))
+}
 </script>
