6431650e99b515244707014df5fad84fc6370ce7
ahmad <ahmed.salamony497@gmail.com>
2023-09-23 07:49:47 +0300

    Change default home main section

---
+++ src/components/HomeMain.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="h-screen w-full bg-white flex justify-center items-center">
+        <span class="text-5xl text-gray-400">Get started Now</span>
+    </div>
+</template>
+
+<script setup>
+
+</script>
